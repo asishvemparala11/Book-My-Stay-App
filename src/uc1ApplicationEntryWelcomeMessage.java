83b39c2a84b218d1318dfb2b330faf61b435efd2
asishvemparala11 <asishvemparala@gmail.com>
2026-03-16 09:20:24 +0530

    UC1: ApplicationEntryWelcomeMessage

--- src/uc1ApplicationEntryWelcomeMessage.java
+++ src/uc1ApplicationEntryWelcomeMessage.java
@@ -1,2 +1,26 @@
-public class uc1ApplicationEntryWelcomeMessage {
-}
+    /**
+     * Book My Stay Application
+     *
+     * This class represents the entry point of the Hotel Booking Management System.
+     * It demonstrates how a Java application starts execution and displays
+     * a welcome message to the user.
+     *
+     * @author Angel
+     * @version 1.0
+     */
+    class UseCase1ApplicationEntryWelcomeMessage {
+
+        /**
+         * Main method - Entry point of the application
+         * @param args command line arguments
+         */
+        public static void main(String[] args) {
+
+            // Print welcome message
+            System.out.println("=================================");
+            System.out.println(" Welcome to Book My Stay ");
+            System.out.println(" Hotel Booking System v1.0 ");
+            System.out.println("=================================");
+            System.out.println("Application started successfully.");
+        }
+    }
